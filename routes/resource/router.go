720ae6a1e241ade358da5a255a70f663fb3346b4
Aryaman <aryamanraj123456789@gmail.com>
2025-03-25 19:04:50 +0530

    updated bruno for resource

--- routes/resource/router.go
+++ routes/resource/router.go
@@ -5,8 +5,8 @@ import "github.com/gofiber/fiber/v2"
 func RegisterRoutes(router fiber.Router) {
 	resource := router.Group("/v1")
 	resource.Post("/", Create)
-	resource.Get("/:id", Get)
 	resource.Get("/search", Search)
+	resource.Get("/:id", Get)
 	resource.Put("/:id", Update)
 	resource.Delete("/:id", Delete)
 }
